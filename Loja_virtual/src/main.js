db48c0c9c0759bdbc9dd340d469effb0e900a99e
mcgmatheus <matheus.mcg@hotmail.com.br>
2023-09-10 21:24:04 -0300

    Criado pagina principal e pagina de carrinho

--- Loja_virtual/src/main.js
+++ Loja_virtual/src/main.js
@@ -1,11 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/routes'
+
+// Vuetify
 import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+
+// Vuex
+import store from './store'
 
 const vuetify = createVuetify({
   components,
@@ -14,6 +19,7 @@ const vuetify = createVuetify({
     defaultSet: 'mdi'
   }
 })
+
 const app = createApp(App)
 
-app.use(router).use(vuetify).mount('#app')
+app.use(router).use(vuetify).use(store).mount('#app')
---
+++ Loja_virtual/src/store/index.js
@@ -0,0 +1,44 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+  state() {
+    return {
+      produtosCarrinho: []
+    }
+  },
+  mutations: {
+    adicionarProdutoAoCarrinho(state, produtoRecebido) {
+      const produtoNoCarrinho = state.produtosCarrinho.find(
+        (produto) => produto.id === produtoRecebido.id
+      )
+
+      if (produtoNoCarrinho) {
+        state.produtosCarrinho = state.produtosCarrinho.map((item) => {
+          if (item.id === produtoRecebido.id) {
+            item.quantidade = item.quantidade + 1
+          }
+          return item
+        })
+      } else {
+        state.produtosCarrinho = [
+          ...state.produtosCarrinho,
+          {
+            ...produtoRecebido,
+            quantidade: 1
+          }
+        ]
+      }
+    }
+  },
+  actions: {
+    alterarNome(context, value) {
+      console.log(value.nome)
+      context.commit('alterarTeste', value.nome)
+    },
+    adicionarProduto(context, value) {
+      context.commit('adicionarProdutoAoCarrinho', value.product)
+    }
+  }
+})
+
+export default store
